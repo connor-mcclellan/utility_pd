985585e580c7db253c7b562e77df3604c9febaef
Brian McClellan <cmcclellan1010@c26a-s24.ufhpc>
2019-05-21 10:10:49 -0400

    added flux histogram code and preliminary histograms for auto and manual convolution

---
+++ output/flux_hist.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Plot a raw image file (.dat format) as a histogram of fluxes.')
+parser.add_argument('filename', metavar='filename', type=str, help='filename of the .dat file')
+parser.add_argument('-o', '--outfile', metavar='outfile', type=str, help='name of output matplotlib savefig file')
+args = parser.parse_args()
+
+if args.outfile is None:
+    outfile = os.path.splitext(args.filename)[0]+'_histogram.png'
+else:
+    outfile = args.outfile
+print('Outfile: '+outfile)
+
+image_data = np.loadtxt(args.filename)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.hist(image_data)
+ax.set_xlabel('Flux (mJy)')
+ax.set_ylabel('n')
+plt.title('Histogram of Fluxes ({})'.format(args.filename))
+fig.savefig(outfile, bbox_inches='tight', dpi=150)
