59b80cfd0e14da15717a4846368c8175d3a1bc4a
Brian McClellan <cmcclellan1010@c32b-s28.ufhpc>
2019-05-22 14:55:20 -0400

    file re reorganization, added lowZ

---
+++ flux_hist.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Plot a raw image file (.dat format) as a histogram of fluxes.')
+parser.add_argument('filename', metavar='filename', type=str, help='filename of the .dat file')
+parser.add_argument('-o', '--outfile', metavar='outfile', type=str, help='name of output matplotlib savefig file')
+args = parser.parse_args()
+
+if args.outfile is None:
+    outfile = os.path.splitext(args.filename)[0]+'_histogram.png'
+else:
+    outfile = args.outfile
+print('Outfile: '+outfile)
+
+image_data = np.ndarray.flatten(np.loadtxt(args.filename))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+nonzero = image_data[image_data != 0.0]
+
+ax.hist(nonzero, bins=np.logspace(np.log10(np.min(nonzero)), np.log10(np.max(nonzero)), 75))
+
+ax.set_xlabel('Flux (mJy)')
+ax.set_xscale('log')
+ax.set_ylabel('n')
+plt.title('{}'.format(args.filename))
+
+if os.path.isfile(outfile):
+    os.remove(outfile)
+fig.savefig(outfile, bbox_inches='tight', dpi=150)
--- output/shell_hists.py
+++
@@ -1,8 +0,0 @@
-from subprocess import call
-
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_0.dat'])
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_1.dat'])
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_2.dat'])
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_3.dat'])
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_4.dat'])
-call(['python', 'flux_hist.py', '/home/cmcclellan1010/pdwork/output/manualconv/progressive/pd_manualconv_progressive_5.dat'])
