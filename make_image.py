965cb8fdc6946eee70a7e835e7f3a9dc448fcfbe
Brian McClellan <cmcclellan1010@c32b-s38.ufhpc>
2019-05-22 15:45:45 -0400

    added make_image.py script with argparse

--- make_image.py
+++ make_image.py
@@ -6,18 +6,29 @@ from hyperion.model import ModelOutput
 from astropy.cosmology import Planck13
 import astropy.units as u
 import os
+import argparse
 
-# ------------------------
-# modifiable header
-# ------------------------
+parser = argparse.ArgumentParser(description='Synthesize an image at either a single wavelength or by convolving many wavelengths with a filter.')
+parser.add_argument('infile', type=str, help='Filename of the ".image" file.')
+parser.add_argument('-w', '--wav', type=float, help='A single wavelength in nm, if producing a monochromatic image.')
+parser.add_argument('-f', '--filterfile', type=str, help='The ".filter" file to be used if convolving multiple wavelengths.')
+parser.add_argument('-d', '--dat', action='store_true', help='If enabled, saves a ".dat" file with image data.')
 
-path = '/home/cmcclellan1010/pdwork/output/lowZ/'
-outpath = '/home/cmcclellan1010/pdwork/output/lowZ/'
-filename = 'example.134.rtout.image'
+args = parser.parse_args()
 
+
+path = os.path.dirname(args.infile)+'/'
+if path == '':
+    path = './'
+filename = os.path.basename(args.infile)
 
 m = ModelOutput(path+filename)
-wav = 0.66 #micron
+
+try:
+    wav = args.wav/1000. #micron
+except TypeError:
+    pass
+
 redshift = 0.001
 image_width = 200 #kpc
 
@@ -42,33 +53,25 @@ w = image.x_max * u.cm
 w = w.to(u.kpc)
 
 
-# Manually combine wavelengths according to filter transmission function
-MANUAL_FILTER_CONVOLUTION = True
-MANUAL_FILTER_CONVOLUTION_FILE = '/home/cmcclellan1010/powderday/filters/STIS_clear_8.filter'
 
-if MANUAL_FILTER_CONVOLUTION:
-    filter_file = np.loadtxt(MANUAL_FILTER_CONVOLUTION_FILE)
+if args.filterfile is not None:
+    # Manually combine wavelengths according to filter transmission function
+    filter_file = np.loadtxt(args.filterfile)
     images = [image.val[0, :, :, i] for i in range(len(image.wav))]
     weights = filter_file[:,1]
     image_data = np.average(images, axis=0, weights=weights)
-    image_suffix = 'manualconv'
+    image_suffix = os.path.splitext(args.filterfile)[0].split('/')[-1]
 else:
     # Find the closest wavelength
-    iwav = np.argmin(np.abs(wav - image.wav))
+    iwav = np.argmin(np.abs(args.wav - image.wav))
     image_data = image.val[0, :, :, iwav]
-    if len(image.wav) == 1:
-        image_suffix = 'autoconv'
-    else:
-        image_suffix = str(int(1000*wav))+'nm'
+    image_suffix = str(int(args.wav))+'nm'
 
-# Save .dat file with image data
-SAVE = True
+outfile = 'pdimageout_'+image_suffix
+outfile_path = path+outfile
 
-output_file_path = outpath+'pd_'+image_suffix+'_'+os.path.splitext(MANUAL_FILTER_CONVOLUTION_FILE)[0].split('/')[-1]
-file_path = 'pd_'+image_suffix+'_'+os.path.splitext(MANUAL_FILTER_CONVOLUTION_FILE)[0].split('/')[-1]
-
-if SAVE:
-    np.savetxt(output_file_path+'.dat', image_data)
+if args.dat:
+    np.savetxt(outfile_path+'.dat', image_data)
 
 #plot the beast
 cax = ax.imshow(np.log(image_data), cmap=plt.cm.viridis, origin='lower', extent=[-w.value, w.value, -w.value, w.value])
@@ -84,5 +87,5 @@ ax.set_xlabel('y kpc')
 
 
 plt.colorbar(cax,label='Flux (mJy)',format='%.0e')
-plt.title(file_path+'.png')
-fig.savefig(output_file_path+'.png', bbox_inches='tight',dpi=150)
+plt.title(outfile+'.png')
+fig.savefig(outfile_path+'.png', bbox_inches='tight',dpi=150)
