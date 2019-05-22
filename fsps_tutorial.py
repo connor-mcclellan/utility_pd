9b0f310de1356004c2428d7944f14c986cfe6166
cmcclellan1010 <cmcclellan1010@ufl.edu>
2019-05-22 14:58:20 -0400

    added histogram for delta func

--- fsps_tutorial.py
+++
@@ -1,9 +0,0 @@
-import fsps
-
-# Initializes a simple stellar population with solar metallicity 
-# of zero, since logzsol = log(z/z_sol) = 0.0 if z = z_sol
-# Some dust, and Calzetti extinction curve
-sp = fsps.StellarPopulation(compute_vega_mags=False, zcontinuous=1,
-                            sfh=0, logzsol=0.0, dust_type=2, dust2=0.2)
-
-
