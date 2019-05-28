614a8d1963914f1f483c06267babd9ddd5d9f312
cmcclellan1010 <cmcclellan1010@ufl.edu>
2019-05-28 19:18:49 -0400

    Created pathch script to simplify pathfinding, incorporated its workflow into make_image

--- make_image.py
+++ make_image.py
@@ -4,6 +4,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from hyperion.model import ModelOutput
 from astropy.cosmology import Planck13
+from pathch import pathch
 import astropy.units as u
 import os
 import argparse
@@ -11,8 +12,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Synthesize an image at either a single wavelength or by convolving many wavelengths with a filter.')
 parser.add_argument('infile', type=str, help='Filename of the ".image" file.')
+parser.add_argument('-o', '--outfile', type=str, help='Path and filename of the outputs.')
 parser.add_argument('-w', '--wav', type=float, help='A single wavelength in nm, if producing a monochromatic image.')
-parser.add_argument('-f', '--filterfile', type=str, help='The ".filter" file to be used if convolving multiple wavelengths.')
+parser.add_argument('-f', '--filter', type=str, help='The ".filter" file to be used if convolving multiple wavelengths.')
 parser.add_argument('-d', '--dat', action='store_true', help='If enabled, saves a ".dat" file with image data.')
 parser.add_argument('--vmin', type=float, help='Minimum of colorbar scale, in units of flux.')
 parser.add_argument('--vmax', type=float, help='Maximum of colorbar scale, in units of flux.')
@@ -20,13 +22,7 @@ parser.add_argument('--vmax', type=float, help='Maximum of colorbar scale, in un
 
 args = parser.parse_args()
 
-
-path = os.path.dirname(args.infile)+'/'
-if path == '':
-    path = './'
-filename = os.path.basename(args.infile)
-
-m = ModelOutput(path+filename)
+m = ModelOutput(pathch(args.infile))
 
 try:
     wav = args.wav/1000. #micron
@@ -57,25 +53,25 @@ w = image.x_max * u.cm
 w = w.to(u.kpc)
 
 
-
-if args.filterfile is not None:
+if args.filter is not None:
     # Manually combine wavelengths according to filter transmission function
-    filter_file = np.loadtxt(args.filterfile)
+    filter_file = np.loadtxt(args.filter)
     images = [image.val[0, :, :, i] for i in range(len(image.wav))]
     weights = filter_file[:,1]
     image_data = np.average(images, axis=0, weights=weights)
-    image_suffix = os.path.splitext(args.filterfile)[0].split('/')[-1]
+    default_image_suffix = os.path.splitext(args.filter)[0].split('/')[-1]
 else:
     # Find the closest wavelength
     iwav = np.argmin(np.abs(args.wav - image.wav))
     image_data = image.val[0, :, :, iwav]
-    image_suffix = str(int(args.wav))+'nm'
+    default_image_suffix = str(int(args.wav))+'nm'
 
-outfile = 'pdimageout_'+image_suffix
-outfile_path = path+outfile
 
+# Save a .dat file, if desired
 if args.dat:
-    np.savetxt(outfile_path+'.dat', image_data)
+    outfile = pathch(args.outfile, 'pdimageout_'+default_image_suffix+'.dat')
+    np.savetxt(outfile, image_data)
+
 
 #plot the beast
 cax = ax.imshow(np.log(image_data), cmap=plt.cm.viridis, origin='lower', 
@@ -93,5 +89,12 @@ ax.set_xlabel('y kpc')
 
 
 plt.colorbar(cax,label='log Flux (mJy)',format='%.0e')
-plt.title(outfile+'.png')
-fig.savefig(outfile_path+'.png', bbox_inches='tight',dpi=150)
+
+plot_outfile = pathch(args.outfile, 'pdimageout_'+default_image_suffix+'.png')
+
+plt.title(plot_outfile)
+fig.savefig(plot_outfile, bbox_inches='tight',dpi=150)
+
+
+
+
---
+++ pathch.py
@@ -0,0 +1,49 @@
+import os
+
+def pathch(path, default=None):
+    """
+    Patch a path. Determine if a provided filename is a path with no filename, 
+    a path with a filename, a path with a filename with the wrong extension, 
+    etc., and make necessary corrections.
+    """
+
+#    if path with no filename:
+#        return path formatted properly
+#    elif path with filename and no extension:
+#        return path with filename (overwriting defaults) and default extension
+#    elif path with filename and wrong extension:
+#        
+    if path is None:
+        path = './'
+
+    if default is None:
+        # The user just wants to format a path properly.
+        if os.path.isdir(path):
+            if path[-1] != '/':
+                path += '/'
+            return path
+        else:
+            return path
+            
+    elif os.path.splitext(default)[1] == '': # Only extension in default arg
+        # The user may specify any filename, but an extension is forced. Remove
+        # any extensions provided by the user and replace with the default.
+
+        if os.path.isdir(path):
+            raise ValueError('A path and extension were provided, but with no filename.')
+
+        path = os.path.splitext(path)[0]+default
+        return path
+
+    else: #Filename and extension in default arg
+        # User may overwrite a default filename if they wish, or provide a path
+        # and the default filename will be used.
+        if os.path.isdir(path):
+            path = os.path.dirname(path+'/')+'/'+default
+        elif os.path.splitext(path)[1] == '': # User provided no extension
+            # Keep user's filename but add an extension
+            path = path + os.path.splitext(default)[1]
+        else: # User provided filename with extension
+            # Force extension
+            path = os.path.splitext(path)[0]+os.path.splitext(default)[1]
+        return path
